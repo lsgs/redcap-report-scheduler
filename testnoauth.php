bc28c91a48c6a8c716e28a7189525eedc6cc1199
Luke <lukestevens@hotmail.com>
2023-05-29 15:04:19 +1000

    Update testnoauth.php

--- testnoauth.php
+++ testnoauth.php
@@ -1,7 +1,8 @@
 <?php
 if ($module->canSendEmail()) {
     $module->cronEntry();
-    echo "View logging in redcap_external_modules_log table for projects where enabled by running this SQL query in the Control Center Database Query Tool:<br>";
+    echo "Test Run Complete";
+    echo "View logging for this test in your redcap_external_modules_log table by running this SQL query in the Control Center Database Query Tool:<br>";
     echo "select eml.* from redcap_external_modules_log eml inner join redcap_external_modules em on eml.external_module_id=em.external_module_id where directory_prefix='report_scheduler' order by log_id desc limit 100<br>";
     echo "done at ".date('Y-m-d H:i:s');
 } else {
